3b17708d8909820d3f5d0e7a43e083769843894c
keshav <kdooleea@yahoo.ca>
2023-11-19 17:15:08 -0500

    feat: completed bin to num dialog

--- src/components/dialogs/bin-num-dialog/bin-num-dialog.component.ts
+++ src/components/dialogs/bin-num-dialog/bin-num-dialog.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import {
+  IDialogContent,
+  IDialogFn,
+} from 'src/interfaces/dialog-content.interface';
 
 @Component({
   selector: 'app-bin-num-dialog',
   templateUrl: './bin-num-dialog.component.html',
-  styleUrls: ['./bin-num-dialog.component.scss']
+  styleUrls: ['./bin-num-dialog.component.scss'],
 })
-export class BinNumDialogComponent {
+export class BinNumDialogComponent implements IDialogFn {
+  getGuideContent(): IDialogContent {
+    return {
+      title: 'Guide',
+    };
+  }
 
+  getGoalContent(): IDialogContent {
+    return {
+      title: 'Goal',
+      description:
+        'Convert binary code on the left screen to number displayed on the right screen.',
+    };
+  }
+
+  getHowContent(): IDialogContent {
+    return {
+      title: 'How to use?',
+      description:
+        'Use the right screen to directly modify the number on it. \n Press the validate button to confirm your answer.',
+    };
+  }
+
+  getNoteContent(): IDialogContent {
+    return {
+      title: 'Note',
+      description:
+        'When the answer is correct, a new binary code is generated. Then, a new round is automatically started after a few seconds.',
+    };
+  }
 }
